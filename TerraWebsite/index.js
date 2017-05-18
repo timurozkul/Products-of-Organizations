2fc2c78c85ff69060c5bb3f9a8fd849ee264f7ae
timurozkul <tim88r@gmail.com>
2017-05-18 14:12:08 +0100

    TerraWebsite: JS carousel complete

---
+++ TerraWebsite/index.js
@@ -0,0 +1,29 @@
+const pauseButton = document.getElementById('container');
+const slides = document.querySelectorAll('.container .slide');
+const slideButtons = document.querySelectorAll('.slide__buttons .button');
+
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
+
+let currentSlide = 0;
+let slideInterval = setInterval(nextSlide,3000);
+
+function nextSlide(onClick) {
+    slides[currentSlide].className = 'slide';
+    slideButtons[currentSlide].className = 'button';
+
+    currentSlide = typeof onClick === 'number' ? onClick : (currentSlide+1)%slides.length;
+
+    slides[currentSlide].className = 'slide showing';
+    slideButtons[currentSlide].className = 'button white';
+}
+
+function pauseSlideshow() { clearInterval(slideInterval); }
+function playSlideshow() { slideInterval = setInterval(nextSlide,2000); }
+
+pauseButton.onmouseover = () => {  pauseSlideshow(); };
+pauseButton.onmouseout = () => {  playSlideshow(); }
+button1.onclick = () => { nextSlide(0); }
+button2.onclick = () => { nextSlide(1); }
+button3.onclick = () => { nextSlide(2); }
